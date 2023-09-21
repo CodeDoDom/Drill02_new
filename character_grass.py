8763145ca78c2bc6d9d6e77b31ffeeaf6792caf2
2022182024 <lhnyoon@tukorea.ac.kr>
2023-09-21 12:35:07 +0900

    사각형 오른쪽 줄 완료

--- character_grass.py
+++ character_grass.py
@@ -33,9 +33,13 @@ def run_rectangle():
     #for x in range(50, 750+1, 5):
         #render_frame(x, 90)#x,y 위치에 캐릭터 그려줄 수 있는 함수
 
-    #top lime
-    for x in range(750, 50-1, -5):
-        render_frame(x, 550)
+    #top line
+    #for x in range(750, 50-1, -5):
+        #render_frame(x, 550)
+
+    #right line
+    for y in range(90, 550+1, 5):
+        render_frame(750, y)
 
 while True:
     #run_circle()
